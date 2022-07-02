63319073605b835111490bdeeef7ade44ffdd23a
meteorlxy <meteor.lxy@foxmail.com>
2022-07-02 13:14:36 +0800

    test: set esbuild target to node14

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  esbuild: {
+    target: 'node14',
+  },
   test: {
     // TODO: vitest coverage with c8 is incorrect
     coverage: {
