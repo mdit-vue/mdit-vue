3dee4fd32b378d68db29dc50616442f31507e1dc
meteorlxy <meteor.lxy@foxmail.com>
2022-07-06 22:00:18 +0800

    test: set alias to run tests with source files

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,18 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 
+const root = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: /^@mdit-vue\/([^/]*)$/,
+        replacement: path.resolve(root, './packages/$1/src/index.ts'),
+      },
+    ],
+  },
   esbuild: {
     target: 'node14',
   },
