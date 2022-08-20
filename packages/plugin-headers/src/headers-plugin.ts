de1030b327f8aa13a809cb2ddf842fcdd3d0d7e5
meteorlxy <meteor.lxy@foxmail.com>
2022-08-21 02:50:40 +0800

    fix(plugin-headers): avoid headers being overridden by another rendering

--- packages/plugin-headers/src/headers-plugin.ts
+++ packages/plugin-headers/src/headers-plugin.ts
@@ -2,7 +2,7 @@ import {
   slugify as defaultSlugify,
   resolveHeadersFromTokens,
 } from '@mdit-vue/shared';
-import type { MarkdownItEnv, MarkdownItHeader } from '@mdit-vue/types';
+import type { MarkdownItEnv } from '@mdit-vue/types';
 import type { PluginWithOptions } from 'markdown-it';
 import type { HeadersPluginOptions } from './types.js';
 
@@ -15,26 +15,16 @@ export const headersPlugin: PluginWithOptions<HeadersPluginOptions> = (
   md,
   { level = [2, 3], slugify = defaultSlugify, format } = {},
 ): void => {
-  let headers: MarkdownItHeader[];
-
-  // push the rule to the end of the chain
-  // resolve headers from the parsed tokens
-  md.core.ruler.push('resolveHeaders', (state) => {
-    headers = resolveHeadersFromTokens(state.tokens, {
+  // extract headers to env
+  const render = md.renderer.render.bind(md.renderer);
+  md.renderer.render = (tokens, options, env: MarkdownItEnv) => {
+    env.headers = resolveHeadersFromTokens(tokens, {
       level,
       shouldAllowHtml: false,
       shouldEscapeText: false,
       slugify,
       format,
     });
-    return true;
-  });
-
-  // extract headers to env
-  const render = md.render.bind(md);
-  md.render = (src, env: MarkdownItEnv = {}) => {
-    const result = render(src, env);
-    env.headers = headers;
-    return result;
+    return render(tokens, options, env);
   };
 };
