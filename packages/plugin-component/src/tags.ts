eb1a2640cfe4675727aede7330d075476bb698d1
meteorlxy <meteor.lxy@foxmail.com>
2024-04-13 13:08:16 +0800

    fix(plugin-component): inline html block tags to make cjs build work

--- packages/plugin-component/src/tags.ts
+++ packages/plugin-component/src/tags.ts
@@ -1,11 +1,73 @@
-import blockNames from 'markdown-it/lib/common/html_blocks.mjs';
-
 /**
  * List of block tags
  *
  * @see https://spec.commonmark.org/0.30/#html-blocks
+ * @see https://github.com/markdown-it/markdown-it/blob/master/lib/common/html_blocks.mjs
  */
-export const TAGS_BLOCK = blockNames;
+export const TAGS_BLOCK = [
+  'address',
+  'article',
+  'aside',
+  'base',
+  'basefont',
+  'blockquote',
+  'body',
+  'caption',
+  'center',
+  'col',
+  'colgroup',
+  'dd',
+  'details',
+  'dialog',
+  'dir',
+  'div',
+  'dl',
+  'dt',
+  'fieldset',
+  'figcaption',
+  'figure',
+  'footer',
+  'form',
+  'frame',
+  'frameset',
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'head',
+  'header',
+  'hr',
+  'html',
+  'iframe',
+  'legend',
+  'li',
+  'link',
+  'main',
+  'menu',
+  'menuitem',
+  'nav',
+  'noframes',
+  'ol',
+  'optgroup',
+  'option',
+  'p',
+  'param',
+  'search',
+  'section',
+  'summary',
+  'table',
+  'tbody',
+  'td',
+  'tfoot',
+  'th',
+  'thead',
+  'title',
+  'tr',
+  'track',
+  'ul',
+];
 
 /**
  * According to markdown spec, all non-block html tags are treated as "inline"
