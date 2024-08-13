f3a9ce7a58c71d3dcea09797ecd66fe9d6d226b7
meteorlxy <meteor.lxy@foxmail.com>
2024-08-13 20:49:16 +0800

    build: bump deps

--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface MarkdownItEnv {
   // to be extended with module augmentation
 }
