df9b2c37e02e10d447e51f3834cac67484ba8bf4
meteorlxy <meteor.lxy@foxmail.com>
2025-06-09 18:04:57 +0800

    build: bump deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,15 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { meteorlxy } from '@meteorlxy/eslint-config';
 
-const packages = fs.readdirSync(
-  path.resolve(fileURLToPath(import.meta.url), '../packages'),
-);
-
 export default meteorlxy({
-  imports: {
-    packageDir: ['./', ...packages.map((item) => `./packages/${item}`)],
+  markdown: {
+    overrides: {
+      'import/order': 'off',
+    },
   },
   typescript: {
     overrides: {
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/prefer-destructuring': 'off',
-      'import/no-rename-default': 'off',
       'prefer-template': 'off',
     },
   },
